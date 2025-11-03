13f2035b2ebb5d76dc2609d8ab4cabd648b4d3a9
Cameron <cameronkaseman@gmail.com>
2025-11-03 00:05:17 -0500

    added titlescreen anim in CODEEEEEEEEE also added some paintings in pontaxtest

--- scenes/levels/release/main_menu/main_menu.gd
+++ scenes/levels/release/main_menu/main_menu.gd
@@ -1,8 +1,12 @@
 extends SceneBase
 
+#i added this to play animated sprite on titlescreen. lord help me.
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	super._ready()
 	%StartGameButton.pressed.connect(func() -> void:
 		SceneManagerAutoload.load_scene(SceneMapping.SceneType.GAME)
 	)
+	#this line too. Call me coder ty.
+	animated_sprite.play("default")
